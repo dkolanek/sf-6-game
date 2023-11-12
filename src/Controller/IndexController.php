ce404e56cef6db16770bd841ff5035be84132c7f
Damian <prywatne81@gmail.com>
2023-11-12 00:01:24 +0000

    mvc and routing

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,19 +12,54 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index.home')]
     public function home(): Response
     {
-     return new Response('Hello World');
+     return $this->render('index/home.html.twig');
     }
 
     #[Route('/about', name: 'index.about')]
     public function about(): Response
     {
-     return new Response('About Page');
+     return $this->render('index/about.html.twig');
     }
 
     #[Route('/hello/{firstName}', name: 'index.hello', methods: ['GET'])]
     public function hello(string $firstName = "Guest"): Response
     {
-     return new Response('<h2>Hello ' . $firstName. '</h2>');
+        $favoriteGames = [
+            'FIFA 22',
+            'Call of Duty: Vanguard',
+            'Battlefield 2042',
+            'Far Cry 6',
+            'Halo Infinite',
+            'Forza Horizon 5',
+            'Dying Light 2',
+            'Back 4 Blood',
+            'Deathloop',
+            'Riders Republic',
+        ];
+     return $this->render('index/hello.html.twig', [
+         'firstName' => $firstName,
+            'favoriteGames' => $favoriteGames,
+     ]);
     }
 
+    #[Route('/top-game', name: 'index.top-game', methods: ['GET'])]
+    public function top(): Response
+    {
+        $topGames = [
+            'FIFA 22',
+            'Call of Duty: Vanguard',
+            'Battlefield 2042',
+            'Far Cry 6',
+            'Halo Infinite',
+            'Forza Horizon 5',
+            'Dying Light 2',
+            'Back 4 Blood',
+            'Deathloop',
+            'Riders Republic',
+        ];
+
+        return $this->render('index/top-game.html.twig', [
+            'topGames' => $topGames,
+        ]);
+    }
 }
